ff1e0d7f69a6f3b151de293b36bf55897e9eaa5a
Barnabas <davidbarnabas666@gmail.com>
2026-03-31 14:22:16 +0100

    feat: add alchemy token balance fetch utility

---
+++ src/utils/alchemy.js
@@ -0,0 +1,21 @@
+export const getTokenBalances = async (address) => {
+  const apiKey = import.meta.env.VITE_ALCHEMY_API_KEY;
+
+  const url = `https://base-mainnet.g.alchemy.com/v2/${apiKey}`;
+
+  const response = await fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      jsonrpc: "2.0",
+      method: "alchemy_getTokenBalances",
+      params: [address],
+      id: 1,
+    }),
+  });
+
+  const data = await response.json();
+  return data.result.tokenBalances;
+};
